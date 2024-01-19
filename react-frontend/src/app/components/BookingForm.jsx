7ec4b1b2bc20e4548ab21557cd5c1e5877ee7348
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-19 16:59:16 +0500

    UI changes in confirm component

--- react-frontend/src/app/components/BookingForm.jsx
+++ react-frontend/src/app/components/BookingForm.jsx
@@ -14,12 +14,17 @@ const BookingForm = ({ date, slot }) => {
     return (
         <div>
             <h3 className='mb-4 text-2xl'>Confirm Booking</h3>
-            
-            <div>
-                <label>Reason for the call:</label>
-                <input type="text" value={reason} onChange={(e) => setReason(e.target.value)} />
+
+            <div className='mb-4'>
+                <input 
+                    type="text" 
+                    value={reason} 
+                    placeholder='Reason for the call'
+                    className='border-solid border-2 border-gray-400 rounded-md px-4 py-2 text-gray-900'
+                    onChange={(e) => setReason(e.target.value)}
+                />
             </div>
-            <button onClick={handleConfirm}>Confirm Call</button>
+            <button className='bg-black text-white p-3 rounded-lg' onClick={handleConfirm}>Confirm Call</button>
 
             {confirmation && (
                 <div>
